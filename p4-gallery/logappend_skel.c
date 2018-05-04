396f5c3285fb8dd71ca3e6e8ae4f04aee87677ae
purrfection <peiranlong@gmail.com>
2018-05-03 20:53:03 -0400

    basic check

--- p4-gallery/logappend_skel.c
+++ p4-gallery/logappend_skel.c
@@ -13,6 +13,16 @@
 #include <openssl/err.h>
 #include <openssl/rand.h>
 
+struct Log{
+  int timestamp;
+  char *token;
+  int is_emp;
+  char *name;
+  int is_arr;
+  int int_room;
+  char *char_room;
+  char *logpath;
+};
 
 int strcheck(char *str, int lc, int uc, int num) {
   int lc_ans = lc, uc_ans = uc, num_ans = num;
@@ -32,14 +42,16 @@ int strcheck(char *str, int lc, int uc, int num) {
   return (lc == lc_ans) && (uc == uc_ans) && (num == num_ans);
 }
 
-int parse_cmdline(int argc, char *argv[]) {
+int parse_cmdline(int argc, char *argv[], struct Log *log) {
 
   int opt = -1;
   int is_good = 1;
-  int timestamp;
+  int is_emp = -1;
+  int is_arr = -1;
   char* logpath;
 
   //pick up the switches
+  /*is_good is set to 0 when arguments are invalid*/
   while ((opt = getopt(argc, argv, "T:K:E:G:ALR:B:")) != -1) {
     switch(opt) {
       case 'B':
@@ -47,12 +59,15 @@ int parse_cmdline(int argc, char *argv[]) {
         break;
 
       case 'T':
+        //timestamp
         /*check if arg contains only 0-9*/
         if(strcheck(argv[optind-1],0,0,1)) {
-          timestamp = atoi(argv[optind-1]);
+          int timestamp = atoi(argv[optind-1]);
           /*check if timestamp within range*/
           if(timestamp < 1 || timestamp > 1073741823) {
             is_good = 0;
+          } else {
+            log->timestamp = timestamp;
           }
         } else {
           is_good = 0;
@@ -61,30 +76,89 @@ int parse_cmdline(int argc, char *argv[]) {
 
       case 'K':
         //secret token
+        /*check if arg is alphanumeric*/
+        if(strcheck(argv[optind-1],1,1,1)) {
+          log->token = argv[optind-1];
+        } else {
+          is_good = 0;
+        }
         break;
 
       case 'A':
         //arrival
+        /*set is_arr to 1*/
+        if(is_arr == -1 || is_arr == 1) {
+          is_arr = 1;
+          log->is_arr = 1;
+        } else {
+          is_good = 0;
+        }
         break;
 
       case 'L':
         //departure
+        /*set is_arr to 0*/
+        if(is_arr == -1 || is_arr == 0) {
+          is_arr = 0;
+          log->is_arr = 0;
+        } else {
+          is_good = 0;
+        }
         break;
 
       case 'E':
         //employee name
+        /*check if arg contains only letters*/
+        if(strcheck(argv[optind-1],1,1,0)) {
+          /*set is_emp to 1*/
+          if(is_emp == -1 || is_emp == 1) {
+            is_emp = 1;
+            log->is_emp = 1;
+            log->name = argv[optind-1];
+          } else {
+            is_good = 0;
+          }
+        } else {
+          is_good = 0;
+        }
         break;
 
       case 'G':
         //guest name
+        /*check if arg contains only letters*/
+        if(strcheck(argv[optind-1],1,1,0)) {
+          /*set is_emp to 0*/
+          if(is_emp == -1 || is_emp == 0) {
+            is_emp = 0;
+            log->is_emp = 0;
+            log->name = argv[optind-1];
+          } else {
+            is_good = 0;
+          }
+        } else {
+          is_good = 0;
+        }
         break;
 
       case 'R':
         //room ID
+        /*check if room id contains only 0-9*/
+        if(strcheck(argv[optind-1],0,0,1)) {
+          int int_room = atoi(argv[optind-1]);
+          /*check if room id is within range*/
+          if(int_room < 0 || int_room > 1073741823) {
+            is_good = 0;
+          } else {
+            log->int_room = int_room;
+          }
+        } else {
+          is_good = 0;
+        }
         break;
 
       default:
         //unknown option, leave
+        is_good = 0;
         break;
     }
 
@@ -103,10 +177,16 @@ int parse_cmdline(int argc, char *argv[]) {
 int main(int argc, char *argv[]) {
 
   int result;
-  result = parse_cmdline(argc, argv);
+  struct Log log;
+  result = parse_cmdline(argc, argv, &log);
 
+  if(result == 0) {
+    printf("invalid\n");
+    exit(255);
+  }
 
-
-
+  printf("Timestamp: %d, Token: %s\n", log.timestamp, log.token);
+  printf("This person is a(n) %s ", log.is_emp==1?"employee":"guest");
+  printf("who just %s room no.%d\n", log.is_arr==1?"arrived":"left",log.int_room);
   return 0;
 }
